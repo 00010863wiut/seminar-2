55fb39ed313b0cafb9420d38b5f24e2e747a9690
00010863wiut <73101164+00010863wiut@users.noreply.github.com>
2020-10-20 11:58:46 +0500

    Create seminar.py

---
+++ seminar.py
@@ -0,0 +1,22 @@
+# 1
+def age_function(age):
+    print(age)
+
+
+# age_function("10")
+# parameter is the "age" written in parenthesis, argument is the number when calling the function
+
+# 2
+var_age = 10
+age_function(var_age)
+# This is variable
+age_function(5+5)
+# and this one is expression
+
+# 3
+def local_variable():
+ my_variable = "some stuff that wont work outside"
+ return
+
+# print(my_variable)
+# It won't work because of the scope!
